540e572f6c62319d7ca60404c2e1e3a1d391fb78
Graham Zuber <grahamzuber@gmail.com>
2016-11-29 16:53:04 -0700

    added tbb includes. changed makefile. still struggling with linker errors.

--- Makefile
+++ Makefile
@@ -2,8 +2,10 @@
 # concurrency_profiler makefile
 #
 
+CC = g++
 CXX = g++
 CXXFLAGS = -std=c++11 -O2 -Wall
+INCLUDES = -ltbb -L/opt/intel/compilers_and_libraries/mac/tbb/lib -I/includes
 
 OBJS = profiler.o
 DOBJS = d_profiler.o
@@ -13,10 +15,10 @@ all: profiler
 debug: d_profiler
 
 profiler: $(OBJS)
-	$(CXX) $(CXXFLAGS) -o profiler $(OBJS)
+	$(CXX) $(CXXFLAGS) -o profiler $(OBJS) $(INCLUDES)
 
 d_profiler: $(DOBJS)
-	$(CXX) $(CXXFLAGS) -g -o d_profiler $(DOBJS)
+	$(CXX) $(CXXFLAGS) $(INCLUDES) -g -o d_profiler $(DOBJS)
 
 profiler.o: profiler.cpp
 
--- profiler.cpp
+++ profiler.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 #include<string>
 #include<unordered_map>
+#include<unistd.h>
+#include<x86intrin.h>
+#include "include/tbb/concurrent_unordered_map.h"
 
 typedef std::unordered_map<std::string, std::string> map;
+typedef tbb::concurrent_unordered_map<std::string, std::string> con_map;
 
 
 map* test()
@@ -22,6 +26,14 @@ void print_map(map* data)
 
 int main(int argc, char* argv[])
 {
+    con_map* test_map = new con_map;
     map* thing = test();
     print_map(thing);
+    uint64_t first = __rdtsc();
+    std::cout << first << std::endl;
+    usleep(1000);
+    uint64_t second = __rdtsc();
+    std::cout << second << std::endl;
+    std::cout << "diff: " << (second - first) << std::endl;
+    
 }
