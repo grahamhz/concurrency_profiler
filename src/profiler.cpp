3744159e8de764667bfd99f47ebf4762b6987c32
Derek Johnson <derekj627@gmail.com>
2016-12-08 00:32:33 -0700

    Basic semaphore structure

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -1,8 +1,10 @@
-#include<iostream>
-#include<string>
-#include<unordered_map>
-#include<unistd.h>
-#include<x86intrin.h>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unistd.h>
+#include <x86intrin.h>
+#include <semaphore.h>
+#include <inttypes.h>
 #include "tbb/concurrent_unordered_map.h"
 #include "frand.h"
 #include "cycles.h"
@@ -10,39 +12,74 @@
 // map related defs
 typedef tbb::concurrent_unordered_map<std::string, std::string> con_map;
 
-con_map* test()
+
+// Needs padding to make each struct 128 
+struct elem {
+    uint64_t val;
+    uint64_t padding;
+};
+
+long NUM_ACCESSES = 1000000;
+int NUM_THREADS = 3;
+
+elem *shared;
+pthread_t *threads;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void * do_it(void *indexp)
 {
-    con_map* data = new con_map;
-    data->emplace("hello", "my name is graham.");
-    return data;
+    int index = *(int*)indexp;
+    
+
+    for (long i = 0; i < NUM_ACCESSES; ++i)
+    {
+        pthread_mutex_lock(&mutex);
+        shared[index].val++;
+        pthread_mutex_unlock(&mutex);
+    }
+
+    return NULL;
 }
 
-void print_map(con_map* data)
+
+void run_basic_test()
 {
-    for(auto iter = data->begin(); iter != data->end(); ++iter)
+    shared = (elem *) calloc(NUM_THREADS, sizeof(elem));
+    threads = (pthread_t *) calloc(NUM_THREADS, sizeof(pthread_t));
+
+    for (int i = 0; i < NUM_THREADS; ++i)
     {
-        std::cout << iter->first << ": " << iter->second << std::endl;
+        int* x = (int *) malloc(sizeof(int));
+        *x = i;
+        pthread_create(&threads[i], NULL, do_it, x);
     }
+
+
+    for (int i = 0; i < NUM_THREADS; ++i)
+    {
+        pthread_join(threads[i], NULL);
+        if(shared[i].val != NUM_ACCESSES)
+            std::cout << "Error. Thread " << i << " only counted to " << shared[i].val << std::endl;
+    }
+
+    printf("\n");
 }
+
 
 int main(int argc, char* argv[])
 {
-    con_map* thing = test();
-    print_map(thing);
+    std::cout << "Testing " << NUM_THREADS << " threads on " << NUM_ACCESSES << " accesses " << std::endl;
     uint64_t first = __rdtsc();
-    std::cout << first << std::endl;
-    usleep(1000000);
-    uint64_t second = __rdtsc();
-    std::cout << second << std::endl;
-    std::cout << "diff: " << (second - first) << std::endl;
+    // std::cout << first << std::endl;
+    
 
-    frand ran(time(NULL));
-    int count = 25;
-    while(count > 0)
-    {
-        std::cout << ran.gen() << std::endl;
-        --count;
-    }
+    run_basic_test();
+
+
+    uint64_t second = __rdtsc();
+    // std::cout << second << std::endl;
+    // std::cout << "diff: " << (second - first) << std::endl;
 
     cycles cycler;
     if(!cycler.init())
@@ -50,8 +87,8 @@ int main(int argc, char* argv[])
         std::cout << "nope. cycles::init failed" << std::endl;
         return 1;
     }
-    std::cout << "cycles per second: " << cycler.get_cycles_per_sec() << std::endl;
-    std::cout << "it took: " << cycler.to_seconds(second - first) << std::endl;
+    // std::cout << "cycles per second: " << cycler.get_cycles_per_sec() << std::endl;
+    std::cout << "took: " << cycler.to_seconds(second - first) << " seconds" << std::endl;
 
 }
 
